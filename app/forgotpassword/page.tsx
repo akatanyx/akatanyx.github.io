2214aaf9fae378fa690e8ba942586fb8d052daee
Daniel Ardi Chandra <danielardichandra01@gmail.com>
2024-10-24 14:11:25 +0700

    Update page.tsx

--- app/forgotpassword/page.tsx
+++ app/forgotpassword/page.tsx
@@ -30,7 +30,6 @@ export default function Login() {
                     id="email"
                     className="bg-black border border-[#C29736] text-[#888888] sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 placeholder-[#888888]"
                     placeholder="Email"
-                    required=""
                   />
                 </div>
               </form>
